e96a0275ee068f413b2a0f7377481e53f27b7c21
yusyun0528 <75973988+yusyun0528@users.noreply.github.com>
2021-03-17 23:00:38 +0900

    Delete employproject directory

--- employproject/employapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import EmployModel ,ShiftModel
-
-# Register your models here.
-
-admin.site.register(EmployModel)
-admin.site.register(ShiftModel)
--- employproject/employapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployappConfig(AppConfig):
-    name = 'employapp'
